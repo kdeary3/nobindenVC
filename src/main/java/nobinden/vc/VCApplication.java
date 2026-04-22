fd64be4096f276ae1c3603c85bea454ec1b506a7
kdeary3 <kenodeary3@gmail.com>
2026-04-22 11:06:21 -0500

    changed name

--- src/main/java/nobinden/vc/VCApplication.java
+++ src/main/java/nobinden/vc/VCApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VcApplication {
+public class VCApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VcApplication.class, args);
+		SpringApplication.run(VCApplication.class, args);
 	}
 
 }
