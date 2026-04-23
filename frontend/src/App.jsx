4800c6e3d8fe127a16943b9cc4f57175c880d314
kdeary3 <kenodeary3@gmail.com>
2026-04-23 10:45:50 -0500

    updated navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import Navbar from "./components/navbar.jsx";
+import NobindenNavbar from "./components/navbar.jsx";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Container} from "react-bootstrap";
+
+import Home from "./pages/home.jsx";
+import About from "./pages/about.jsx";
+import Partners from "./pages/partners.jsx";
+import Portfolio from "./pages/portfolio.jsx";
+import Apply from "./pages/apply.jsx";
 
 const App = () => {
     return (
-    <>
-        <Navbar />
-    </>
-)};
+        <Router>
+            <NobindenNavbar /> {/* Just the visual bar */}
+            <Container fluid className="px-5 py-4">
+                <Routes> {/* All logic lives here */}
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/partners" element={<Partners />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/apply" element={<Apply />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Container>
+        </Router>
+    );
+};
 
 export default App
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,49 +1,38 @@
-// dependency imports
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import { Routes, Route } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-// page imports
 import Home from "../pages/home.jsx";
 import About from "../pages/about.jsx";
 import Partners from "../pages/partners.jsx";
 import Portfolio from "../pages/portfolio.jsx";
 import Apply from "../pages/apply.jsx";
 
-
 const NobindenNavbar = () => {
     return (
-        <Router>
+        <> {/* Use a fragment instead of a Router here */}
             <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
                 <Container fluid className="px-5">
-                    <Navbar.Brand as={Link} to="/home">nobindenVC</Navbar.Brand>
+                    <LinkContainer to="/home">
+                        <Navbar.Brand>nobindenVC</Navbar.Brand>
+                    </LinkContainer>
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" >
+                    <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            <Nav.Link as={Link} to="/partners">Partners</Nav.Link>
+                            <LinkContainer to="/home"><Nav.Link>Home</Nav.Link></LinkContainer>
+                            <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
+                            <LinkContainer to="/partners"><Nav.Link>Partners</Nav.Link></LinkContainer>
 
                             <NavDropdown title="Startups" id="basic-nav-dropdown">
-                                <NavDropdown.Item as={Link} to="/portfolio">Our Portfolio</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/apply">Apply</NavDropdown.Item>
+                                <LinkContainer to="/portfolio"><NavDropdown.Item>Our Portfolio</NavDropdown.Item></LinkContainer>
+                                <LinkContainer to="/apply"><NavDropdown.Item>Apply</NavDropdown.Item></LinkContainer>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
-            <Container fluid className="px-5">
-                <Routes>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/partners" element={<Partners />} />
-                    <Route path="/portfolio" element={<Portfolio />} />
-                    <Route path="/apply" element={<Apply />} />
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </Container>
-        </Router>
+        </>
     );
 };
 
