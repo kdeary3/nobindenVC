5eec5e760a9013e619b00fd2d775a7e59ce9b8dd
kdeary3 <kenodeary3@gmail.com>
2026-04-23 09:50:14 -0500

    pushed character accidentally

--- src/test/java/nobinden/vc/startup/StartupServiceTest.java
+++ src/test/java/nobinden/vc/startup/StartupServiceTest.java
@@ -4,7 +4,7 @@ import nobinden.vc.partner.Partner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-\import org.mockito.InjectMocks;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
