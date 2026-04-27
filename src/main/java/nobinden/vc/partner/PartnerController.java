19d0b3983bd4be43c9d6ab718189f5740ba1e55f
kdeary3 <kenodeary3@gmail.com>
2026-04-27 10:39:57 -0500

    updated endpoint

--- src/main/java/nobinden/vc/partner/PartnerController.java
+++ src/main/java/nobinden/vc/partner/PartnerController.java
@@ -4,6 +4,7 @@ import nobinden.vc.startup.Startup;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
