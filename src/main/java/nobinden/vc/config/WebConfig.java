13b1ed1ac55ca508387c92e9d30222fb3ad47f8d
kdeary3 <kenodeary3@gmail.com>
2026-04-27 10:58:16 -0500

    webconfig file

---
+++ src/main/java/nobinden/vc/config/WebConfig.java
@@ -0,0 +1,18 @@
+package nobinden.vc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173") // will become nobinden.com
+                // .allowedOrigins("http://localhost:5173", "https://nobinden.com") will become nobinden.com
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
