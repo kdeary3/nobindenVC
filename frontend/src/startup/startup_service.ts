8112426f7c4f990489ed5b37602a17e072d5f6f6
kdeary3 <kenodeary3@gmail.com>
2026-04-27 11:22:15 -0500

    created axiosUpdateStartupStage function

--- frontend/src/startup/startup_service.ts
+++ frontend/src/startup/startup_service.ts
@@ -1,9 +1,12 @@
 import axios, {type AxiosResponse} from 'axios'
 import type {Startup} from './startup_type'
 
+// This is the frontend service.
+
 type AxiosGetStartups = () => Promise<Startup[]>
 type AxiosSaveStartup = (startup: Startup) => Promise<Startup>
 type AxiosDeleteStartup = (id: number) => Promise<void>
+type AxiosUpdateStartupStage = (id: number, stage: string) => Promise<Startup>
 
 export const axiosGetAllStartups: AxiosGetStartups = async () => {
     return axios
@@ -23,10 +26,22 @@ export const axiosSaveStartup: AxiosSaveStartup = (startup: Startup) => {
 
 export const axiosDeleteStartup: AxiosDeleteStartup = (id: number) => {
     return axios
-        .delete('/api/v1/startup/' + id)
+        .delete(`/api/v1/startup/${id}`)
         .then((response: AxiosResponse<void>) => response.data)
         .catch((error: unknown) => {
             console.error('Failed to delete startup:', error);
+            throw error;
+        });
+}
+
+export const axiosUpdateStartupStage: AxiosUpdateStartupStage = (id: number, stage: string) => {
+    return axios
+        .patch(`/api/v1/startup/${id}/stage`, stage, {
+            headers: { 'Content-Type': 'text/plain'}
+        })
+        .then((response: AxiosResponse<void>) => response.data)
+        .catch((error: unknown) => {
+            console.error('Failed to update startup stage:', error);
             throw error;
         });
 }
