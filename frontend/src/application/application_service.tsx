13102bec6d29cc8f95488f8846c9092e699d0d16
kdeary3 <kenodeary3@gmail.com>
2026-04-28 11:57:21 -0500

    frontend service for applicaiton

---
+++ frontend/src/application/application_service.tsx
@@ -0,0 +1,47 @@
+import {Application} from "./application_type";
+import axios, {AxiosResponse} from "axios";
+
+type AxiosGetAllApplications = () => Promise<Application[]>
+type AxiosSaveApplication = (application: Application) => Promise<Application>
+type AxiosDeleteApplication = (id: number) => Promise<void>
+type AxiosUpdateApplication = (id: number, application: Application) => Promise<Application>
+
+export const axiosGetAllApplications: AxiosGetAllApplications = () => {
+    return axios
+        .get('/api/v1/application')
+        .then((response: AxiosResponse<Application[]>) => response.data)
+        .catch((error: unknown) => {
+            console.error('Failed to get applications:', error)
+            throw error
+        })
+}
+
+export const axiosSaveApplication: AxiosSaveApplication = (application: Application) => {
+    return axios
+        .post('/api/v1/application', application)
+        .then((response: AxiosResponse<Application>) => response.data)
+        .catch((error: unknown) => {
+            console.error('Failed to save application:', error)
+            throw error
+        })
+}
+
+export const axiosDeleteApplication: AxiosDeleteApplication = (id: number) => {
+    return axios
+        .delete(`/api/v1/application/${id}`)
+        .then((response: AxiosResponse<void>) => response.data)
+        .catch((error: unknown) => {
+            console.error('Failed to delete application:', error)
+            throw error
+        })
+}
+
+export const axiosUpdateApplication: AxiosUpdateApplication = (id: number, application: Application) => {
+    return axios
+        .patch(`/api/v1/application/${id}`, application)
+        .then((response: AxiosResponse<Application>) => response.data)
+        .catch((error: unknown) => {
+            console.error('Failed to update application:', error);
+            throw error;
+        });
+}
---
+++ frontend/src/application/application_type.tsx
@@ -0,0 +1,13 @@
+
+export type Application = {
+    id: number
+    name: string
+    sector: string
+    founders: string[]
+    founderEmail: string
+    founderPhoneNumber: string
+    deckUrl: string
+    additionalComments: string
+    status: string
+    submittedAt: string
+}
