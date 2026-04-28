2a9776fc65e968c3c4b5bb6142e59705030f0070
kdeary3 <kenodeary3@gmail.com>
2026-04-28 11:34:14 -0500

    completed application respository, service, controller

--- src/main/java/nobinden/vc/application/ApplicationController.java
+++ src/main/java/nobinden/vc/application/ApplicationController.java
@@ -1,4 +1,52 @@
 package nobinden.vc.application;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.NoSuchElementException;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/startup/application")
 public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Application saveNewApplication(@RequestBody Application application) {
+        return applicationService.saveApplication(application);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Application> findApplicationById(@PathVariable Long id) {
+        try {
+            Application application = applicationService.findApplicationById(id);
+            return ResponseEntity.ok(application);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public List<Application> getApplications() {
+        return applicationService.findAllApplications();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Application> deleteApplicationById(@PathVariable Long id) {
+        try {
+            applicationService.deleteApplicationById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- src/main/java/nobinden/vc/application/ApplicationService.java
+++ src/main/java/nobinden/vc/application/ApplicationService.java
@@ -1,4 +1,36 @@
 package nobinden.vc.application;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ApplicationService {
+
+    private final ApplicationRepository applicationRepository;
+
+    public ApplicationService(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    public Application saveApplication(Application application) {
+        return applicationRepository.save(application);
+    }
+
+    public List<Application> saveAllApplications(List<Application> applications) {
+        return applicationRepository.saveAll(applications);
+    }
+
+    public Application findApplicationById(Long id) {
+        return applicationRepository.findById(id).orElseThrow();
+    }
+
+    public List<Application> findAllApplications() {
+        return applicationRepository.findAll();
+    }
+
+    public void deleteApplicationById(Long id) {
+        applicationRepository.deleteById(id);
+    }
+
 }
