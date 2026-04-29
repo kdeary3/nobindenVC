8efc5d22b31063274fa0d87f8fa4e7cd769fb111
kdeary3 <kenodeary3@gmail.com>
2026-04-29 10:53:29 -0500

    edit about text

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -4,7 +4,7 @@ const About = () => {
         <>
             <h1>About Page</h1>
             <h2>About Us</h2>
-            <p>We are three brothers, Keno, Korbin and Kasden, who share a deep passion for technology, innovation, finance, and venture capital. All three of us have had the honor of attending the United States Military Academy at West Point (Classes of 2023, 2026, 2028), which has shaped our discipline, leadership, and drive for excellence. Our mission is to leverage our skills and experiences to innovate and invest in the tech and startup industries.</p>
+            <p>We are three brothers, Keno, Korbin and Kasden, who share a deep passion for technology, innovation, finance, and venture capital. Our mission is to leverage our skills and experiences to innovate and invest in the tech and startup industries.</p>
             <h2>The Meaning Behind "nobinden"</h2>
             <p>The name "nobinden" is derived from the last few letters of each of our names: Ke<b>NO</b>, Kor<b>BIN</b>, and Kas<b>DEN</b>. It also carries the hidden meaning of "no bending," symbolizing the unbreakable bond we share as brothers. This name encapsulates our unity and our commitment to stand firm together in all our ventures.</p>
             <h2>Our Investments</h2>
