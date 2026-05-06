248ce857f1fb590391bcee70cf7f1c49d1c6e84e
kdeary3 <kenodeary3@gmail.com>
2026-05-06 12:02:45 -0500

    fixed logo and nvc name

--- frontend/src/pages/navbar.tsx
+++ frontend/src/pages/navbar.tsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
 const NobindenNavbar = () => {
-    const [logoErr, setLogoErr] = useState(false);
 
     return (
         <Navbar expand="lg" sticky="top" className="nb-navbar">
             <Container fluid className="px-4">
                 <LinkContainer to="/">
                     <Navbar.Brand>
-                        {!logoErr
-                            ? <img src="/images/logo.png" alt="nobindenVC" onError={() => setLogoErr(true)}  />
-                            : 'nobindenVC'
-                        }
+                        <img src="/images/logo.png" alt="nobindenVC"/>
+                        &nbsp; nobindenVC
                     </Navbar.Brand>
                 </LinkContainer>
 
-                <Navbar.Toggle aria-controls="main-nav" />
+                <Navbar.Toggle aria-controls="main-nav"/>
                 <Navbar.Collapse id="main-nav">
                     <Nav className="ms-auto align-items-center gap-3">
                         <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
